e4d43d2057058c9b4359e19fea39a49e68a98560
ynportfolios <y.n.portfolios@gmail.com>
2021-01-21 11:37:35 +0900

    タイムゾーンの修正

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,15 @@ Bundler.require(*Rails.groups)
 
 module CloneF
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
